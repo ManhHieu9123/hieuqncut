15c98bac856f928cdf4865fed32b9fff58269cc3
ManhHieu9123 <chumanhhieuvg@gmail.com>
2024-10-18 10:41:19 +0700

    first commit

---
+++ app/src/main/java/com/example/ketnoidjango/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ketnoidjango;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("tel:")) {
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
+        webView.loadUrl("http://127.0.0.1:8000/");
+
+    }
+
+
+
+
+}
